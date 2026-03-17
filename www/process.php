3d0eecc8505108a2dcc1aa24e84647097866931c
mrsalute39 <120706814+mrsalute39@users.noreply.github.com>
2026-03-17 20:29:45 +0200

    Updated process.php(STAGE 4)
    -обновил process.php

--- www/process.php
+++ www/process.php
@@ -1,13 +1,26 @@
 <?php
 session_start();
 $username = htmlspecialchars($_POST['username']);
-$count_order = htmlspecialchars($_POST['age'] ?? '');
+$age = htmlspecialchars($_POST['age'] ?? '');
+$theme = htmlspecialchars($_POST['theme'] ?? '');
+$prize = isset($_POST['prize']) ? 1 : 0;
+$difficulty = htmlspecialchars($_POST['difficulty'] ?? '');
 
 $errors = [];
-if(empty($username)) $errors[] = "Имя не может быть пустым";
-if(!filter_var($count_order, FILTER_VALIDATE_INT)) $errors[] = "Некорректный возраст";
+if (empty($username)) {
+    $errors[] = "Имя не может быть пустым";
+}
+if (!filter_var($age, FILTER_VALIDATE_INT)) {
+    $errors[] = "Неверно указан возраст";
+}
+if (empty($theme)) {
+    $errors[] = "Тема должна быть выбрана";
+}
+if (empty($difficulty)) {
+    $errors[] = "Выберите сложность";
+}
 
-if(!empty($errors)){
+if (!empty($errors)) {
     $_SESSION['errors'] = $errors;
     header("Location: index.php");
     exit();
@@ -16,7 +29,7 @@ if(!empty($errors)){
 require_once 'ApiClient.php';
 $api = new ApiClient();
 
-$url = 'https://www.themealdb.com/api/json/v1/1/random.php'; 
+$url = 'https://opentdb.com/api.php?amount=5'; 
 $apiData = $api->request($url);
 
 $_SESSION['api_data'] = $apiData;
@@ -24,7 +37,14 @@ $_SESSION['api_data'] = $apiData;
 
 $_SESSION['username'] = $username;
 $_SESSION['age'] = $age;
-$line = $username . ";" . $age . "\n";
+$line = $username . ";" . $age . ";" . $theme . ";" . $prize . ";" . $difficulty . "\n";
 file_put_contents("data.txt", $line, FILE_APPEND);
+
+require 'db.php';
+require 'Student.php';
+
+$order = new Student($pdo);
+$order->add($username, $age, $theme, $prize, $difficulty);
+
 header("Location: index.php");
 exit();
